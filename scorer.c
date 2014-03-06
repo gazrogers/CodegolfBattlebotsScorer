38f09fca476ca4a4634a8c1bb4244f5a85dc3611
Gareth Rogers <gareth@garethrogers.net>
2014-03-06 11:08:29 +0000

    Changes

--- scorer.c
+++ scorer.c
@@ -4,19 +4,28 @@
 
 #define NUMBOTS 2
 #define BOUTSPERMATCH 5
-#define ROUNDSPERBOUT 1000
+#define ROUNDSPERBOUT 1
 #define MAXFILENAMESIZE 100
+#define MAXWEAPONS 100
+
+void outputarena(char arena[10][10]);
 
 int main()
 {
     int bot1,bot2,bout,round,bot1x,bot1y,bot2x,bot2y,bot1energy,bot2energy;
+    int loop;
     char bots[NUMBOTS][MAXFILENAMESIZE]={"./donowt", "php -f huggybot.php"};
+    char argumentstring[1000];
     int matcheswon[NUMBOTS],boutswon[NUMBOTS];
-    int missiles[100][3];
-    int bullets[100][3];
-    char blankarena[10][10]={'.'};
+    int missiles[MAXWEAPONS][3]={-1};
+    int bullets[MAXWEAPONS][3]={-1};
+    int landmines[MAXWEAPONS][2]={-1};
+    int paralyzedturnsremaining=0;
     char arena[10][10];
 
+    memset(missiles, -1, sizeof(missiles));
+    memset(bullets, -1, sizeof(bullets));
+    memset(landmines, -1, sizeof(landmines));
     srand(time(NULL));
 
     for(bot1=0;bot1<NUMBOTS-1;bot1++)
@@ -26,9 +35,8 @@ int main()
             printf("%s vs %s ",bots[bot1],bots[bot2]);
             for(bout=0;bout<BOUTSPERMATCH;bout++)
             {
-                printf("%d ",bout);
+                printf("Bout %d\n",bout);
                 //setup the arena for the bout
-                memcpy(arena, blankarena, sizeof(a));
                 bot1x=0;
                 bot2x=9;
                 bot1y=rand()%10;
@@ -36,7 +44,24 @@ int main()
                 bot1energy=bot2energy=10;
                 for(round=0;round<ROUNDSPERBOUT;round++)
                 {
-//draw the arena based on current state
+                    //draw the arena based on current state
+                    memset(arena, '.', sizeof(arena));
+                    for(loop=0;loop<MAXWEAPONS;loop++)
+                    {
+                        if(missiles[loop][0]!= -1)
+                        {
+                            arena[missiles[loop][0]][missiles[loop][1]]='M';
+                        }
+                        if(bullets[loop][0]!= -1)
+                        {
+                            arena[bullets[loop][0]][bullets[loop][1]]='B';
+                        }
+                        if(landmines[loop][0]!= -1)
+                        {
+                            arena[landmines[loop][0]][landmines[loop][1]]='L';
+                        }
+                    }
+                    
 //send the arena to both bots to get the commands
 //interpret commands
 //do bot movement phase
@@ -46,5 +71,18 @@ int main()
             }
             printf("\n");
         }
+    }
+}
+
+void outputarena(char arena[10][10])
+{
+    int i,j;
+    for(j=0;j<10;j++)
+    {
+        for(i=0;i<10;i++)
+        {
+            printf("%c", arena[i][j]);
+        }
+        printf("\n");
     }
 }
